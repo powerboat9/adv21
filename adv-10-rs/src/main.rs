b06638d9a58a72a6f3db590e500da96149c6a0f3
powerboat9 <powerboat9.gamer@gmail.com>
2021-12-10 01:15:00 -0500

    Trimmed down code

--- adv-10-rs/src/main.rs
+++ adv-10-rs/src/main.rs
@@ -1,110 +1,8 @@
-#![feature(maybe_uninit_uninit_array)]
-#![feature(maybe_uninit_array_assume_init)]
-#![feature(mixed_integer_ops)]
-#![feature(cell_update)]
-#[macro_use]
-extern crate lazy_static;
-
-use std::borrow::Borrow;
-use std::cell::{Cell, RefCell};
-use std::collections::{BTreeMap, BTreeSet, HashMap, HashSet, VecDeque};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::iter::{from_fn, Peekable};
-use std::mem::MaybeUninit;
-use std::ops::Deref;
-use std::rc::Rc;
-use std::sync::atomic::{AtomicUsize, Ordering};
+use std::iter::{from_fn};
 
 const FILENAME: &'static str = "i1.txt";
-
-fn chunk_count<V, const N: usize>(mut it: impl Iterator<Item=V>) -> impl Iterator<Item=[V; N]> {
-    from_fn(move || {
-        let mut ret = MaybeUninit::uninit_array();
-        for pos in 0..N {
-            if pos == 0 {
-                ret[pos] = MaybeUninit::new(it.next()?);
-            } else {
-                ret[pos] = MaybeUninit::new(it.next().unwrap());
-            }
-        }
-        unsafe {
-            Some(MaybeUninit::array_assume_init(ret))
-        }
-    })
-}
-
-fn take_number(it: &mut Peekable<impl Iterator<Item=char>>) -> Option<u32> {
-    let mut acc = 0;
-    let has_n = false;
-    while let Some(c) = it.next_if(|v| v.is_ascii_digit()) {
-        acc *= 10;
-        acc += (c as u32) - ('0' as u32);
-    }
-    if has_n {
-        Some(acc)
-    } else {
-        None
-    }
-}
-
-fn take_white_numbers(it: &mut Peekable<impl Iterator<Item=char>>) -> Vec<u32> {
-    from_fn(|| {
-        take_ignore_white(it, take_number)
-    }).collect()
-}
-
-fn map_nonwhite_chunks(mut it: impl Iterator<Item=char>) -> impl Iterator<Item=String> {
-    let mut acc = None;
-    let mut cont = true;
-    from_fn(move || {
-        if !cont {
-            return None
-        }
-        loop {
-            match it.next() {
-                Some(c) if c.is_ascii_whitespace() => {
-                    if acc.is_some() {
-                        return acc.take();
-                    }
-                },
-                Some(c) => {
-                    acc.get_or_insert(String::new()).push(c);
-                }
-                None => {
-                    cont = false;
-                    return acc.take()
-                }
-            }
-        }
-    })
-}
-
-fn take_ignore_white<I: Iterator<Item=char>, V>(it: &mut Peekable<I>, tk_fn: impl FnOnce(&mut Peekable<I>) -> V) -> V {
-    take_any_white(it);
-    (tk_fn)(it)
-}
-
-fn take_any_white(it: &mut Peekable<impl Iterator<Item=char>>) {
-    while let Some(_) = it.next_if(|v| v.is_ascii_whitespace()) {
-    }
-}
-
-fn expect_char(it: &mut Peekable<impl Iterator<Item=char>>, c: char) -> Result<(), ()> {
-    it.next_if_eq(&c).map(|v| ()).ok_or(())
-}
-
-fn take_nonwhite(it: &mut Peekable<impl Iterator<Item=char>>) -> Option<String> {
-    let mut s = String::new();
-    while let Some(c) = it.next_if(|v| v.is_ascii_whitespace()) {
-        s.push(c)
-    }
-    if s.len() != 0 {
-        Some(s)
-    } else {
-        None
-    }
-}
 
 fn read_lines() -> impl Iterator<Item = String> {
     let file = File::open(FILENAME).unwrap();
@@ -150,7 +48,7 @@ enum Action {
 }
 
 fn main() {
-    let mut data = read_data();
+    let data = read_data();
 
     let mut res_1 = 0;
     let mut line_scores = Vec::new();
