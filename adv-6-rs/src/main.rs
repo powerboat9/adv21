093377e0351a7c445e0434a8e9e49c5ec47620a8
powerboat9 <powerboat9.gamer@gmail.com>
2021-12-26 15:10:19 -0500

    Optimised day 6

--- adv-6-rs/src/main.rs
+++ adv-6-rs/src/main.rs
@@ -1,140 +1,125 @@
-#![feature(map_first_last)]
-#![feature(int_abs_diff)]
-#[macro_use]
-extern crate lazy_static;
-extern crate regex;
+#![feature(generic_const_exprs)]
 
-use std::cmp::Ordering;
-use std::collections::{BTreeMap, BTreeSet, HashMap, HashSet};
-use std::collections::hash_map::Entry;
 use std::fs::File;
-use std::io::{BufRead, BufReader, Read};
-use std::iter::{from_fn, FusedIterator};
+use std::io::Read;
 use std::mem::swap;
-use std::ops::{Add, AddAssign, Div, Sub};
-
-use regex::Regex;
+use std::ops::{Add, Sub};
 
 const FILENAME: &'static str = "i1.txt";
-lazy_static! {
-    static ref LINE_RE: Regex = Regex::new(r"\d+").unwrap();
-}
 
 fn read_data() -> Vec<i32> {
     let mut file = File::open(FILENAME).unwrap();
     let mut data = String::new();
     file.read_to_string(&mut data).unwrap();
-    LINE_RE.find_iter(data.as_str()).map(|v| v.as_str().parse().unwrap()).collect()
+    data.trim_end().split(',').map(|s| s.parse().unwrap()).collect()
 }
 
-#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Hash)]
-struct Vec2i {
-    x: i32,
-    y: i32
+#[derive(Copy, Clone, PartialEq, Eq, Hash, Debug)]
+struct Matrix<const R: usize, const C: usize> where [(); R * C]: Sized {
+    backing: [i128; R * C]
 }
 
-impl Add for Vec2i {
+impl<const R: usize, const C: usize> Add for Matrix<R, C> where [(); R * C]: Sized {
     type Output = Self;
 
     fn add(self, rhs: Self) -> Self::Output {
-        Vec2i {
-            x: self.x + rhs.x,
-            y: self.y + rhs.y
+        let mut ret = [0; R * C];
+        for i in 0..(R * C) {
+            ret[i] = self.backing[i] + rhs.backing[i]
+        }
+        Matrix::<R, C> {
+            backing: ret
         }
     }
 }
 
-impl AddAssign for Vec2i {
-    fn add_assign(&mut self, rhs: Self) {
-        *self = self.add(rhs)
-    }
-}
-
-impl Sub for Vec2i {
+impl<const R: usize, const C: usize> Sub for Matrix<R, C> where [(); R * C]: Sized {
     type Output = Self;
 
     fn sub(self, rhs: Self) -> Self::Output {
-        Vec2i {
-            x: self.x - rhs.x,
-            y: self.y - rhs.y
+        let mut ret = [0; R * C];
+        for i in 0..(R * C) {
+            ret[i] = self.backing[i] - rhs.backing[i]
+        }
+        Matrix::<R, C> {
+            backing: ret
         }
     }
 }
 
-impl Vec2i {
-    fn unit_diff(&self) -> Self {
-        let d = gcd(self.x.unsigned_abs(), self.y.unsigned_abs());
-        Vec2i {
-            x: self.x / (d as i32),
-            y: self.y / (d as i32)
+impl<const RC: usize> Matrix<RC, RC> where [(); RC * RC]: Sized {
+    fn ident() -> Self {
+        let mut backing = [0; RC * RC];
+        for i in 0..RC {
+            backing[i * (RC + 1)] = 1;
         }
-    }
-
-    fn new(x: i32, y: i32) -> Self {
-        Vec2i {
-            x,
-            y
+        Matrix {
+            backing
         }
     }
 }
 
-fn gcd(mut a: u32, mut b: u32) -> u32 {
-    if a < b {
-        swap(&mut a, &mut b)
-    }
-    if a == 0 {
-        return 1;
-    }
-    while b != 0 {
-        a = a % b;
-        swap(&mut a, &mut b);
-    }
-    a
-}
-
-#[derive(PartialEq, Eq, Clone, Copy)]
-struct Line {
-    start: Vec2i,
-    end: Vec2i,
-}
-
-impl Line {
-    fn get_points(self) -> impl Iterator<Item = Vec2i> {
-        let u_delta = (self.end - self.start).unit_diff();
-        let mut pos = self.start;
-        from_fn(move || {
-            if pos > self.end {
-                None
-            } else {
-                let r = pos;
-                pos += u_delta;
-                Some(r)
+fn matrix_multiply<
+    const A: usize,
+    const B: usize,
+    const C: usize>
+(
+    a: &Matrix<A, B>,
+    b: &Matrix<B, C>
+) -> Matrix<A, C> where
+    [(); A * B]: Sized,
+    [(); B * C]: Sized,
+    [(); A * C]: Sized
+{
+    let mut ret = [0; A * C];
+    for row in 0..A {
+        for col in 0..C {
+            ret[row * C + col] = 0;
+            for i in 0..B {
+                ret[row * C + col] += a.backing[row * B + i] * b.backing[i * C + col];
             }
-        })
+        }
+    }
+    Matrix::<A, C> {
+        backing: ret
     }
 }
 
-impl PartialOrd<Self> for Line {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
+fn matrix_pow<const A: usize>(m: &Matrix<A, A>, e: usize) -> Matrix<A, A> where [(); A * A]: Sized {
+    if e == 0 {
+        Matrix::ident()
+    } else if e == 1 {
+        m.clone()
+    } else {
+        let mut sub = matrix_pow(m, e >> 1);
+        sub = matrix_multiply(&sub, &sub);
+        if (e & 1) != 0 {
+            sub = matrix_multiply(&sub, m)
+        }
+        sub
     }
 }
 
-impl Ord for Line {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.start.cmp(&other.start)
-    }
-}
+const ONE_SIM: Matrix<9, 9> = Matrix {
+    backing: [
+        0, 1, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 1, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 1, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 1, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 1, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 1, 0, 0,
+        1, 0, 0, 0, 0, 0, 0, 1, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 1,
+        1, 0, 0, 0, 0, 0, 0, 0, 0
+    ]
+};
 
 fn sim_for(data: &mut [i128; 9], time: usize) {
-    for _ in 0..time {
-        let tick_over = data[0];
-        for i in 0..8 {
-            data[i] = data[i+1];
-        }
-        data[6] += tick_over;
-        data[8] = tick_over;
-    }
+    let m_data = Matrix::<9, 1> {
+        backing: *data
+    };
+    let mut res = matrix_multiply(&matrix_pow(&ONE_SIM, time), &m_data);
+    swap(data, &mut res.backing);
 }
 
 fn sum_data(data: &[i128; 9]) -> i128 {
